05710044d7891b6d0705ec982f9c8161094ed70b
Gaby Dias <36906363+gabydias@users.noreply.github.com>
2021-11-25 03:14:09 -0300

    Criação da App Python/Flask

---
+++ test.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from app import app
+import unittest
+
+class Test(unittest.TestCase):
+    def setUp(self):
+        # cria uma instância do unittest, precisa do nome "setUp"
+        self.app = app.test_client()
+
+        # envia uma requisicao GET para a URL
+        self.result = self.app.get('/')
+
+    def test_requisicao(self):
+        # compara o status da requisicao (precisa ser igual a 200)
+        self.assertEqual(self.result.status_code, 200)
+
+    def test_conteudo(self):
+        # verifica o retorno do conteudo da pagina
+        self.assertEqual(self.result.data.decode('utf-8'), "Hello World")

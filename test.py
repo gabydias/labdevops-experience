9d766a17b5ffe490dbf4a2a01a1c49dfdb27a5dd
fel-albuquerque <40171352+fel-albuquerque@users.noreply.github.com>
2022-05-21 08:04:13 -0300

    ajuste

--- test.py
+++ test.py
@@ -17,3 +17,4 @@ class Test(unittest.TestCase):
     def test_conteudo(self):
         # verifica o retorno do conteudo da pagina
         self.assertEqual(self.result.data.decode('utf-8'), "Hello World")
+        
